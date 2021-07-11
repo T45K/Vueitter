e4101ed2d505f37fa9df98cf7b9c9c38034cb952
T45K <tasktas9@gmail.com>
2021-07-11 19:13:16 +0900

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Editor />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Editor from "@/components/Editor.vue";
 
 @Component({
   components: {
-    HelloWorld,
+    Editor,
   },
 })
 export default class App extends Vue {}
